4eb6a07ff2d0d82988651baa62716dcb045333a4
Futureprogrammerali <34901825+Futureprogrammerali@users.noreply.github.com>
2021-09-25 22:28:04 +0100

    memo
    asd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+ 
 import './App.css';
 
-function App() {
+import React ,{useMemo, useState}   from 'react'
+
+ 
+function App() { 
+ const [Number, setNumberstate] = useState(0)
+ function setnumber (e){
+
+  setNumberstate(e.target.value)
+ }
+ const changestyle = () => {
+    
+
+}
+
+
+const heavyfunction = (2) => {
+  for (let i = 0; i < 1000000000; i++) {
+  }
+return x * x
+}
+ 
+
+ 
+const squareVal = useMemo(()=> {return heavyfunction(Number)},[Number])
+//const squareVal =  heavyfunction(Number)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>the number is {Number}</h1> 
+    <h1>Square {squareVal}</h1> 
+    <br/>
+    <input type="number"  value={Number} onChange={setnumber}></input>
+    
+    <button  onClick={changestyle}> Change style</button>
     </div>
   );
 }

5e9b57b52724c6c0f1e57ce6b2c47a6585de0752
Futureprogrammerali <34901825+Futureprogrammerali@users.noreply.github.com>
2021-09-28 12:10:30 +0100

    app

--- src/App.js
+++ src/App.js
@@ -6,32 +6,11 @@ import React ,{useMemo, useState}   from 'react'
  
 function App() { 
  const [Number, setNumberstate] = useState(0)
- function setnumber (e){
-
-  setNumberstate(e.target.value)
- }
- const changestyle = () => {
-    
-
-}
-
-
-const heavyfunction = (2) => {
-  for (let i = 0; i < 1000000000; i++) {
-  }
-return x * x
-}
- 
-
- 
-const squareVal = useMemo(()=> {return heavyfunction(Number)},[Number])
-//const squareVal =  heavyfunction(Number)
-
   return (
     <div className="App">
-    <h1>the number is {Number}</h1> 
-    <h1>Square {squareVal}</h1> 
-    <br/>
+    <h1>Test use state {Number}</h1> 
+   
+ 
     <input type="number"  value={Number} onChange={setnumber}></input>
     
     <button  onClick={changestyle}> Change style</button>
